9ae7b91165ca5547c2e4dd54f935d729645a520d
Alejandro Segovia Gallardo <asega1996@gmail.com>
2019-09-19 13:26:10 +0200

    feat : Musician controller completed + minnors changes

--- src/musician/Musician.controller.ts
+++ src/musician/Musician.controller.ts
@@ -1,25 +1,18 @@
 import * as mongoose from 'mongoose';
 import { Request, Response } from 'express';
 import MusicianService from '../musician/Musician.service'
+import { Musician } from './Musician.interface';
 //import { OK } from 'http-status-codes'
 
 
 class MusicianController {
 
-
     constructor() { }
 
     public retrieveAll(req: Request, res: Response): void  {
-        console.log(MusicianService)
-        MusicianService.retrieveAll({}).then(result => {
+        MusicianService.retrieveAll(req.query).then(result => {
             res.status(200).json(result);
         }).catch(err => console.log(err));
-        /*this.musicianService.retrieveAll(req.query).then(result => {
-            
-            (res)? res.status(200).json(result) : console.log('petazo')
-    
-        }).catch(err => console.log(err));*/
-
     }
 
     public retrieveById(req: Request, res: Response): void  {
@@ -30,10 +23,30 @@ class MusicianController {
         MusicianService.retrieveById(req.params.id).then(result => {
             res.status(200).json(result);
         }).catch(err => console.log(err));
-
     }
 
-    
+    public create(req: Request, res: Response): void {
+        let musician = req.body as Musician;
+        MusicianService.create(musician).then(result => {
+            res.status(200).json(result);
+        }).catch(err => console.log(err));
+    }
+
+    public delete(req: Request, res: Response): void {
+        const id: string = req.params.id;
+        MusicianService.delete(id).then(result => {
+            res.status(200).json(result);
+        }).catch(err => console.log(err));
+    }
+
+    public update(req: Request, res: Response): void {
+        const id = req.params.id;
+        const musician = req.body as Musician
+        MusicianService.update(id,musician).then(result => {
+            res.status(200).json(result);
+        }).catch(err => console.log(err));
+    }
+
 }
 
 export default new MusicianController();
